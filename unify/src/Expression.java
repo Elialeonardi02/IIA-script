b70c45e68626c206a7189547bc431923d1698b8e
Elialeonardi02 <e.leonardi5@studenti.unipi.it>
2024-03-15 20:06:35 +0100

    test UNIFY

--- unify/src/Expression.java
+++ unify/src/Expression.java
@@ -5,12 +5,41 @@ public class Expression{
     List<String> listExp = new ArrayList<String>();
 
     public Expression(String e){
-        String[] splitted = e.split(",");
+        String[] splitted = e.split(";");
+        int numParentesi = 0;
+        String res = "";
         for (String s : splitted) {
-            listExp.add(s);
-            //System.out.println(s);
+            numParentesi = countOccurrences(s,'(') - countOccurrences(s,')');
+            if (numParentesi == 0){ // se numero parentesi è 0
+                if(res.equals(""))
+                    listExp.add(s);
+                else{
+                    listExp.add(res + "," + s);
+                    res = "";
+                }
+            } else {
+                if (res.equals(""))
+                    res = s;
+                else
+                    res = res + "," + s;
+            }
         }
     }
+    //x,y,H(x),F(x,G(x,y))
+    /*
+    x
+    y
+    H(x) -> 0
+    F(x -> 1
+    G(x -> 2
+    y)) -> 0
+     */
+    // F(G(x,y),x)
+    /*
+    F(G(x
+    y)
+    x)
+     */
 
     public boolean equals(Expression e){
         if(listExp.size() != e.listExp.size()) return false;
@@ -18,6 +47,15 @@ public class Expression{
             if(!listExp.get(i).equals(e.listExp.get(i))) return false;
         }
         return true;
+    }
+    public static int countOccurrences(String str, char targetChar) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == targetChar) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
